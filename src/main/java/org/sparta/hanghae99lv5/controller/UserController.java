cde526b77a747fcc04cf7b19276413ce1296c1ec
KimDabomi <cdabomi@nate.com>
2024-01-09 15:01:15 +0900

    feat: 의존성 주입 -> 생성자 주입

--- src/main/java/org/sparta/hanghae99lv5/controller/UserController.java
+++ src/main/java/org/sparta/hanghae99lv5/controller/UserController.java
@@ -3,16 +3,21 @@ package org.sparta.hanghae99lv5.controller;
 import org.sparta.hanghae99lv5.dto.UserRequestDto;
 import org.sparta.hanghae99lv5.message.SuccessMessage;
 import org.sparta.hanghae99lv5.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/join")
     public ResponseEntity<String> createUser(@RequestBody UserRequestDto requestDto) {
